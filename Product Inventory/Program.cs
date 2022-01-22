b75bb48f506307f1af1910442f8371e0b961c109
SergeiAB <1974bsa@mail.ru>
2022-01-22 17:17:33 +0300

    Добавьте файлы проекта.

---
+++ Product Inventory/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using InventoryLibrary;
+
+namespace Product_Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ведомость по инвентаризации";
+            Product product = new Product();
+            product.PrintConsol();
+            Console.Read();
+        }
+    }
+}
