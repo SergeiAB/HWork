109ccef4ffc73dd5f96602e9a1a976111f972db4
SergeiAB <1974bsa@mail.ru>
2022-01-27 11:07:22 +0300

    Изменил управление на кнопках

--- Product Inventory/Program.cs
+++ Product Inventory/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using InventoryLibrary;
+using System;
 using System.Collections.Generic;
-using InventoryLibrary;
 
 namespace Product_Inventory
-{   
+{
     public class Program
     {
         public enum Menu
@@ -17,14 +17,14 @@ namespace Product_Inventory
             Esc,//отменить зменения
             Null
         }
-        
+
         static string Path = $"{Environment.CurrentDirectory}\\InventDataList.json";
-        
+
         static void Main(string[] args)
         {
             List<Product> SheetProduct = new();
             FileIOService FileIoService = new(Path);
-            Inventori inventori=new Inventori();
+            Inventori inventori = new Inventori();
             Menu menu = Menu.Load;
             Console.Title = "Ведомость по инвентаризации";
             string msgError = "Ошибка ввода, попробуйте еще раз:";
@@ -37,12 +37,12 @@ namespace Product_Inventory
                         {
                             Console.WriteLine(menu.ToString());
                             SheetProduct = FileIoService.LoadData();
-                            if(SheetProduct.Count == 0)
+                            if (SheetProduct.Count == 0)
                             {
                                 Console.WriteLine("В списке товаров нет записей !!!\nДобавте новую запись в список:");
-                               
-                                menu=Menu.Add;
-                               
+
+                                menu = Menu.Add;
+
                                 continue;
                             }
                             else
@@ -55,25 +55,25 @@ namespace Product_Inventory
                     case Menu.Add:
                         {
                             Console.WriteLine(menu.ToString());
-                            
+
 
                             Console.WriteLine("Введите наименование товара, не более 15 символов:");
-                            string NameProduct=Console.ReadLine();
-                            
+                            string NameProduct = Console.ReadLine();
+
                             NameProduct = IsTextNullLen(NameProduct, msgError);
-                            
+
                             Console.WriteLine("Введите количество едениц товара:");
                             string NumUnit = Console.ReadLine();
-                           
-                            double NumberUnits = CheckEnterNumber(NumUnit, msgError,0.001);
-                            
-                            
+
+                            double NumberUnits = CheckEnterNumber(NumUnit, msgError, 0.001);
+
+
                             Console.WriteLine("Введите стоимость еденицы товара:");
                             string PriceUnit = Console.ReadLine();
-                            
-                            decimal UnitPrice = (decimal)CheckEnterNumber(PriceUnit, msgError,0.01);
-                           
-                            SheetProduct.Add(new Product(NameProduct, UnitPrice,NumberUnits ,SheetProduct));
+
+                            decimal UnitPrice = (decimal)CheckEnterNumber(PriceUnit, msgError, 0.01);
+
+                            SheetProduct.Add(new Product(NameProduct, UnitPrice, NumberUnits, SheetProduct));
                             inventori.PrintSheet(SheetProduct);
                             break;
                         }
@@ -122,43 +122,44 @@ namespace Product_Inventory
 
                 menu = GetMenu();
 
-               
-            }while (menu!=Menu.Close);
- 
+
+            } while (menu != Menu.Close);
+
         }
 
 
         //Вывод на консоль клавиш управления програмой
         static void MenuShow()
-        {   Console.WriteLine("--МЕНЮ"+new String('-',28));
-            Console.WriteLine("| Добавить новую запись: CTRL+N   "+"|\n" +
-                "| Удалить запись: Delete          "+"|\n" +
-                "| Показать список товаров: CTRL+P "+"|\n" +
-                "| Сохранить изменения: CTRL+S     "+"|\n" +
-                "| Закрыть программу: CTRL+Q       "+"|\n" +
-                "| Загрузить файл: CTRL+L          "+"|");
-            Console.Write(new String('-',34));
+        {
+            Console.WriteLine("--МЕНЮ" + new String('-', 28));
+            Console.WriteLine("| Добавить новую запись: CTRL+A   " + "|\n" +
+                "| Удалить запись: CTRL+D          " + "|\n" +
+                "| Показать список товаров: CTRL+P " + "|\n" +
+                "| Сохранить изменения: CTRL+S     " + "|\n" +
+                "| Закрыть программу: CTRL+Q       " + "|\n" +
+                "| Загрузить файл: CTRL+L          " + "|");
+            Console.Write(new String('-', 34));
             Console.WriteLine();
         }
 
         //Проверка наименования
         static string IsTextNullLen(string Text, string Messag)
-        {   
-            Text=Text.Trim(' ','\t');
+        {
+            Text = Text.Trim(' ', '\t');
             while (String.IsNullOrEmpty(Text))
             {
                 Console.WriteLine(Messag);
-                Text=Console.ReadLine().Trim(' ','\t');
+                Text = Console.ReadLine().Trim(' ', '\t');
             }
             Text = Text.Trim().ToLower();
             if (Text.Length > 15)
             {
-                Text=Text.Substring(0,15);
+                Text = Text.Substring(0, 15);
             }
-                //написание с большой буквы
+            //написание с большой буквы
             char tmp = char.ToUpper(Text[0]);
-            Text= Text.Substring(1,Text.Length-1);
-            return$"{tmp}{Text}";
+            Text = Text.Substring(1, Text.Length - 1);
+            return $"{tmp}{Text}";
 
         }
 
@@ -178,7 +179,7 @@ namespace Product_Inventory
                 }
 
             } while (!flag);
-            return Math.Round(number,3);
+            return Math.Round(number, 3);
         }
 
         static int CheckEnterNumber(string enterText, string message, int minValue)
@@ -188,7 +189,7 @@ namespace Product_Inventory
             do
             {
                 flag = int.TryParse(enterText, out number);
-                if (!flag||number<minValue)
+                if (!flag || number < minValue)
                 {
                     Console.WriteLine(message);
                     flag = false;
@@ -196,7 +197,7 @@ namespace Product_Inventory
                 }
 
             } while (!flag);
-                return number;
+            return number;
         }
 
         // перехват клавиш управления
@@ -209,12 +210,15 @@ namespace Product_Inventory
             ConsoleKeyInfo keyEnter = Console.ReadKey(true);
             switch (keyEnter.Key)
             {
-                case ConsoleKey.Delete:
+                case ConsoleKey.D:
                     {
-                        menu = Menu.Delete;
+                        if ((keyEnter.Modifiers & ConsoleModifiers.Control) != 0)
+                        {
+                            menu = Menu.Delete;
+                        };
                         break;
                     }
-                case ConsoleKey.N:
+                case ConsoleKey.A:
                     {
                         if ((keyEnter.Modifiers & ConsoleModifiers.Control) != 0)
                         {
